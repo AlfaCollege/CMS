0ea883c3250e514e51ff142b9f5bce5bdf0da702
xr6reactions <wouter.stulp@gmail.com>
2015-05-28 08:53:25 +0200

    menu dynamisch gemaakt

---
+++ assets/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">WaterGoldFish</a>
+    </div>
+    <!-- Top Menu Items -->
+    <ul class="nav navbar-right top-nav">
+
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Welkom Gebruiker<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li class="divider"></li>
+                <li>
+                    <a href="#"><i class="fa fa-cutlery"></i> Het Witte Paard</a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <a href="#"><i class="glyphicon glyphicon-knight"></i> Wobbesheerd</a>
+
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav side-nav">
+            <li class= "<?php if($currentpage =='index'){echo 'active';}?>">
+                <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+            </li>
+            <li class="<?php if($currentpage =='reserveringen'){echo 'active';}?>">
+                <a href="reserveringen.php"><i class=" glyphicon glyphicon-credit-card"></i> Reserveringen</a>
+            </li>
+            <li class="<?php if($currentpage =='menu'){echo 'active';}?>">
+                <a href="menu.php"><i class="glyphicon glyphicon-pushpin"></i> Menu</a>
+            </li>
+            <li class="<?php if($currentpage =='pagina'){echo 'active';}?>">
+                <a href="pagina.php"><i class="glyphicon glyphicon-pushpin"></i> Pagina's</a>
+            </li>
+            <li class="<?php if($currentpage =='recenties'){echo 'active';}?>">
+                <a href="recenties.php"><i class="glyphicon glyphicon-pushpin"></i> Recenties</a>
+            </li>
+        </ul>
+        </li>
+        </ul>
+    </div>
+</body>
+</html>
--- recenties.php
+++ recenties.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -34,72 +35,10 @@
 <div id="wrapper">
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">WaterGoldFish</a>
-        </div>
-        <!-- Top Menu Items -->
-        <ul class="nav navbar-right top-nav">
-
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Welkom Gebruiker<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li class="divider"></li>
-                    <li>
-                        <a href="#"><i class="fa fa-cutlery"></i> Het Witte Paard</a>
-                    </li>
-                    <li class="divider"></li>
-                    <li>
-                        <a href="#"><i class="glyphicon glyphicon-knight"></i> Wobbesheerd</a>
-
-                    </li>
-                    <li class="divider"></li>
-                    <li>
-                        <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav side-nav">
-                <li class="active">
-                    <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                </li>
-                <li>
-                    <a href="reserveringen.php"><i class=" glyphicon glyphicon-credit-card"></i> Reserveringen</a>
-                </li>
-                <li>
-                    <a href="menu.php"><i class="glyphicon glyphicon-pushpin"></i> Menu</a>
-                </li>
-                <li>
-                    <a href="pagina.php"><i class="fa fa-file-text-o"></i> Pagina's</a>
-                </li>
-
-
-                <!--<li>
-                    <a href="#"><i class="fa fa-fw fa-edit"></i> Forms</a>
-                </li>
-                <li>
-                    <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-                    <ul id="demo" class="collapse">
-                        <li>
-                            <a href="#">Dropdown Item</a>
-                        </li>
-                        <li>
-                            <a href="#">Dropdown Item</a>
-                        </li>-->
-            </ul>
-            </li>
-            </ul>
-        </div>
+    <?php
+    $currentpage = "recenties";
+    require_once 'assets/header.php';
+    ?>
         <!-- /.navbar-collapse -->
     </nav>
 
