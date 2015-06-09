debb30306ee7f496bd7e67649087b6047dcb12ce
koudbier <Rubenbeeftink@hotmail.nl>
2015-06-09 14:05:55 +0200

    login changed

--- libs/auth.php
+++ libs/auth.php
@@ -5,37 +5,52 @@
  * Date: 27/05/2015
  * Time: 10:52
  */
-
+session_start();
 
 include_once('database.php');
 
-class auth {
+class auth
+{
 
-    public function __construct() {
-        $db = new DB_Class; //nog aanpassen aan database
-    }
+    public $user_data = array();
+    public $_SESSION;
+    public $errors = array();
 
-    public function login($username, $password) {
+    public function login()
+    {
 
-        $username = $_POST['username'];
-        $password = $_POST['password'];
 
-        //query
-        $result = mysql_query("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
-        $user_data = mysql_fetch_array($result);
-        $no_rows = mysql_num_rows($result);
+        if (empty($_POST['username'])) {
+            $this->errors[] = "er is geen username ingevuld.";
+        } elseif (empty($_POST['password'])) {
+            $this->errors[] = "er is geen wachtwoord ingevuld.";
+        } elseif (!empty($_POST['username']) && !empty($_POST['password'])) {
 
-        //login
-        if ($no_rows == 1) {
-            $_SESSION['login'] = true;
-            $_SESSION['id'] = $user_data['id'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
 
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public function get_session(){
-        return $_SESSION['login'];
+
+            //query
+            $result = DB::select('*', 'gebruikers', 'Naam = $username AND wachtwoord = $password');
+            $this->user_data = mysql_fetch_array($result);
+            $no_rows = mysql_num_rows($result);
+
+            print_r($username);
+
+            //login
+            if ($no_rows == 1) {
+                $_SESSION['login'] = true;
+                $_SESSION['id'] = $this->user_data['ID'];
+                $_SESSION['username'] = $this->user_data['Naam'];
+
+                return true;
+            } else {
+                echo('de ingevoerde gegevens kloppen niet.');
+                return false;
+            }
+        } return true;
+
+    } public function get_session(){
+            return $_SESSION['login'];
     }
 }
