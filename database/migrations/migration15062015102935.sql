0b6b17bef9b0e2fcbf348286ee0b0639ad4e38b4
Daniel Gelling <danielgelling@MacBook-Air-van-Daniel.local>
2015-06-15 10:40:00 +0200

    Added reservations table

---
+++ database/migrations/migration15062015102935.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `reservering`;
+
+CREATE TABLE IF NOT EXISTS `reservations` (
+	`id` int(11) auto_increment unique,
+	`first_name` varchar(255),
+	`last_name` varchar(255),
+	`email` varchar(255),
+	`phone_number` varchar(255),
+	`datetime` datetime,
+	`extras` text
+);
