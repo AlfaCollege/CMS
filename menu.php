4b6021b787bbe8291d73b0c3f61d89f58b6e6758
Daniel Gelling <danielgelling@MacBook-Air-van-Daniel.local>
2015-06-16 13:59:21 +0200

    Removed require upload.php

--- menu.php
+++ menu.php
@@ -14,9 +14,6 @@ if(isset($_GET['delete'])){
 <html lang="en">
 
 <head>
-    <? require("upload.php");
-    ?>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
