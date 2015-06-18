d3e9865d7aa78ebcf7d5f1c6e50eb78742df1f2f
Berwout <tehber@hotmail.com>
2015-06-18 12:40:46 +0200

    Nieuwe auth library gemaakt. Login en logout werkend gemaakt. Database bijgewerkt zodat je kan inloggen met admin, admin.

---
+++ libs/auth.old.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ruben
+ * Date: 27/05/2015
+ * Time: 10:52
+ */
+
+include_once('database.php');
+include_once('Redirect.php');
+include_once('validation.php');
+
+if(isset($_POST['submit'])) {
+   $auth = new Auth;
+    $auth->login();
+}
+
+class auth
+{
+
+    public $user_data;
+    public $_SESSION;
+    public $errors;
+    public function login()
+    {
+
+        if (isset($_POST['submit'])) {
+
+            if (empty($_POST['username'])) {
+                $this->errors[] = "er is geen username ingevuld.";
+            } elseif (empty($_POST['password'])) {
+                $this->errors[] = "er is geen wachtwoord ingevuld.";
+            } elseif (!empty($_POST['username']) && !empty($_POST['password'])) {
+
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+
+                $username = 'Ruben';
+                $password = 'Ruben';
+
+                //query
+                $result = DB::select('*', 'gebruikers', 'Naam = ' . $username . 'AND Wachtwoord = ' . $password);
+                var_dump($result);
+                die();
+                $this->user_data = mysql_fetch_array($result);
+                $no_rows = mysql_num_rows($result);
+
+                //login
+                if ($no_rows == 1) {
+                    $_SESSION['login'] = true;
+                    $_SESSION['id'] = $this->user_data['ID'];
+                    $_SESSION['username'] = $this->user_data['Naam'];
+
+                    Redirect::to('index.php');
+                    return true;
+                } else {
+                    echo('de ingevoerde gegevens kloppen niet.');
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        return false;
+
+    }
+    public function get_session(){
+
+    return $_SESSION['login'];
+}
+}
--- logout.php
+++ logout.php
@@ -1,7 +1,9 @@
 <?php
 
 session_start();
-session_destroy();
+unset($_SESSION['logged_in']);
+unset($_SESSION['UID']);
+unset($_SESSION['Naam']);
 
 header('Location: login.php');
 
