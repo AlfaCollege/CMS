7a2dbf1e0119a9c900eaaaa0dfd6f604e194cd04
Berwout <tehber@hotmail.com>
2015-06-22 09:50:45 +0200

    Nieuwe database update.

--- data/reserveringen.php
+++ data/reserveringen.php
@@ -1,45 +1,92 @@
 <?php
 include "../libs/database.php";
-$voornaam = $achternaam = $email = $telefoonnummer = $personen = $voorkeurstijd = $datum = $extrainfo = "";
-$minname = "2";
-$max = "255";
-$maxpersonen = 3;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $voornaam = test_input($_POST["voornaam"]);
-    $achternaam = test_input($_POST["achternaam"]);
-    $email = test_input($_POST["email"]);
-    $telefoonnummer = test_input($_POST["telefoonnummer"]);
-    $personen = test_input($_POST["personen"]);
-    $voorkeurstijd = test_input($_POST["voorkeurstijd"]);
-    $datum = test_input($_POST["datum"]);
-    $extrainfo = test_input($_POST["extrainfo"]);
+function validate($var){
+    //
 }
 
-
-
-elseif ($voornaam < $minname) {
-    echo "naam moet op zijn minst 2 letters zijn";
-} elseif ($voornaam < $max) {
-    echo "naam moet minder dan 255 letters zijn";
-}
-elseif ($achternaam < $minname){
-    echo "achternaam moet op zijn minst 2 letters zijn";
-}
-elseif($achternaam < $max){
-    echo "achternaam moet minder dan 255 letters zijn";
-}
-elseif($personen < $maxpersonen){
-    echo "achternaam moet minder dan 3 cijfers zijn";
-}
-
-
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+if (!empty($_POST)){
+    //
+    $voornaam       = $_POST['voornaam'];
+    $achternaam     = $_POST['achternaam'];
+    $email          = $_POST['email'];
+    $telefoonnummer = $_POST['telefoonnummer'];
+    $personen       = $_POST['personen'];
+    $voorkeurstijd  = $_POST['voorkeurstijd'];
+    $datum          = $_POST['datum'];
+    $extrainfo      = $_POST['extra'];
+    $url            = $_POST['url'];
+} else {
+    //
+    die(json_encode(
+            [
+                'Error'     => 500,
+                'Message'   => 'Er is geen data opgegeven'
+            ]
+        ));
 }
 
-DB::insert("voornaam,achternaam,email,telefoonnumer,aantal,extra,datum,tijd", "reserveringen", ["voornaam" =>$voornaam, "achternaam" => $achternaam, "email" => $email, "telefoonnummer" => $telefoonnummer, "aantal" => $personen, "extra" => $extrainfo, "datum" => $datum, "tijd" => $voorkeurstijd,]);
+/*
+    TODO:
+        DONE - Kijk of er geen duplicaat van de resevering bestaat.
+        DONE - ALS hij wel bestaat stuur ze terug naar waar ze vandaan kwamen met een bericht.
+        - Anders valideer de waarden die mee gestuurt zijn.
+*/
+$reseveringen   = DB::select('*', 'reservering', 'voornaam = \''.$voornaam.'\'');
+
+if (empty($reseveringen)){
+    //
+    //Geen duplicaat
+    if (!empty($voornaam) && (count_chars($voornaam) > 3)){
+        //
+        //Voornaam is legit.
+        if (!empty($achternaam) && (count_chars($voornaam) > 3)){
+            //
+            //Achternaam is legit.
+            if (!empty($email) && (count_chars($email) > 4)){
+                //
+                //Email is legit
+                if (!empty($telefoonnummer) && (count_chars($telefoonnummer) < 16)){
+                    //
+                    //Tel.Nr. is legit
+                    if (!empty($personen) && $personen > 1){
+                        //
+                        //Personen is legit
+                        if (!empty($voorkeurstijd)){
+                            //
+                            //Voorkeurstijd is legit
+                            if (!empty($datum)){
+                                //
+                                //Datum is legit.
+                                if (!empty($url)){
+                                    //
+                                    //URL is legit.
+                                    DB::insert('voornaam,achternaam,email,telefoonnummer,personen,voorkeurstijd,personen,datum,extra', 'reservering', [
+                                            'voornaam'          => $voornaam,
+                                            'achternaam'        => $achternaam,
+                                            'email'             => $email,
+                                            'telefoonnummer'    => $telefoonnummer,
+                                            'personen'          => $personen,
+                                            'voorkeurstijd'     => $voorkeurstijd,
+                                            'datum'             => $datum,
+                                            'extra'             => $extra
+                                        ]);
+
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+} else {
+    //
+    die(json_encode([
+            'Error'     => 500,
+            'Message'   => 'De reservering bestaat al.'
+        ]));
+}
+
+return false;
