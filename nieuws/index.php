f7d63e7646a3620a523b3185ba4641264084891e
Berwout <tehber@hotmail.com>
2015-06-24 13:11:32 +0200

    Fix in nieuws

--- nieuws/index.php
+++ nieuws/index.php
@@ -1,18 +1,31 @@
 <?php
+header('Content-type: application/JSON');
 include('../libs/database.php');
 
-$data   = DB::select('*', 'artikelen');
+$data = DB::select('*', 'artikelen');
 
-$count  = 1;
-$json_data[0] = $count;
-foreach($data as $nieuws){
-    //
-    $json_data[$count] = [
-        'ID'    => $nieuws['id'],
-        'Title' => $nieuws['titel'],
-        'Text'  => $nieuws['text']
-    ];
+if ($data === false){
+	//
+	echo json_encode([
+			'Error' 	=> 304,
+			'Message' 	=> 'Geen data gevonden voor de opgegeven query.'
+		]);
+	exit();
 }
+
+$count 			= 1;
+$json_data[0] 	= 0;
+
+foreach ($data as $content) {
+	//
+	$json_data[$count] = [
+			'ID' 			=> $content['id'],
+			'Title' 			=> $content['titel'],
+			'Text' 		=> $content['text'],
+	];
+	$count++;
+}
+
 $json_data[0] = $count-1;
 
-die(json_encode($json_data));
+echo json_encode($json_data);
