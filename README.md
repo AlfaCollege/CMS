c1287c434b42b5a303216a521ed172875daf8393
KIPdeKIP <kipdekip@gmail.com>
2015-11-13 14:48:51 +0100

    del

--- README.md
+++
@@ -1,2 +0,0 @@
-# HetWittePaard
-De website staat opgeslagen op [archive.org](https://web.archive.org/web/20150915070013/http://www.caferestauranthetwittepaard.nl/).
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-	'host' 			=> 'db.caferestauranthetwittepaard.nl',
-	'type' 			=> 'mysql',
-	'dbname' 		=> 'md296091db331475',
-	'user' 			=> 'md296091db331475',
-	'password' 		=> 'zRZyI9sP'
-];
